162cb46e28e81fc4017c198237e3fb4c8aed3d90
nous23 <cchen23@outlook.com>
2021-01-26 15:52:00 +0800

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/alecthomas/kingpin"
+	"github.com/kardianos/service"
 
 	pkglog "servicemanager/pkg/log"
 	"servicemanager/pkg/manager"
 	"servicemanager/pkg/util"
 )
+
+var action string
+
+func init() {
+	kingpin.Flag("action", "start|stop|restart|install|uninstall").Default("").StringVar(&action)
+}
 
 func main() {
 	kingpin.Parse()
@@ -19,6 +28,14 @@ func main() {
 	m, err := manager.New()
 	if err != nil {
 		util.ExitOnError(err)
+	}
+
+	if action != "" {
+		err = service.Control(m, action)
+		if err != nil {
+			util.ExitOnError(fmt.Errorf("%s %s failed: %v", action, manager.Name, err))
+		}
+		util.ExitOnSuccess(fmt.Sprintf("%s %s success", action, manager.Name))
 	}
 	err = m.Run()
 	if err != nil {
